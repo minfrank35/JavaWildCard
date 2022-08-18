88c0d6dc08a85fb065152ccb2fa87f2682663b83
minfrank <minfrank35@gmail.com>
2022-08-18 12:57:01 +0900

    제네릭 PECS 실습

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+//        arrayTest();
+//        genericTest();
 
+        List<MyGrandParent> list = new ArrayList<>();
+
+        printCollection2(list);
+
+        addElement(list);
     }
+
+    /**
+     * 배열 같은경우는 공변
+     */
+    static void arrayTest() {
+        Integer[] integers = new Integer[]{1, 2, 3};
+        printArray(integers);
+    }
+
+    static void printArray(Object[] arr) {
+        for (Object e : arr) {
+            System.out.println(e);
+        }
+    }
+    /**
+     * 제네릭은 공변
+     */
+    static void genericTest() {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+//        printCollection(list);   // 컴파일 에러 발생
+    }
+
+
+    static void printCollection(Collection<Object> c) {
+        for (Object e : c) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * 와일드 카드 <?> unknown type
+     * @param c
+     */
+    static void printCollection2(Collection<?> c) {
+        for (Object e : c) {
+            System.out.print(e + " ");
+        }
+    }
+
+    static void addElement(Collection<? super MyParent> c) {
+        c.add(new MyChild());
+        c.add(new MyParent());
+    }
+
+
+}
+
+
+class MyGrandParent {
+
+}
+
+class MyParent extends MyGrandParent {
+
+}
+
+class MyChild extends MyParent {
+
 }
